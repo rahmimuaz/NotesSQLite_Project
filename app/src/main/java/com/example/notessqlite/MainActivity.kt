2623b2e4cba8b251cf950cfc1b0d30cff26cfeaa
IT22266750 <IT22266750@my.sliit.lk>
2024-05-12 10:09:56 +0530

    first change

---
+++ app/src/main/java/com/example/notessqlite/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.notessqlite
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.notessqlite.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    //change1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addButton.setOnClickListener{
+            val intent = Intent(this,AddNoteActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
